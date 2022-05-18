784876b391615a10145dcd5de830ae901b60d63a
dimyth <oladimejifolakunmi@yahoo.com>
2022-05-18 11:37:07 -0500

    Initial Commit

---
+++ SourceControlXcode122/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SourceControlXcode122
+//
+//  Created by Charles Oladimeji on 5/18/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SourceControlXcode122/SourceControlXcode122App.swift
@@ -0,0 +1,17 @@
+//
+//  SourceControlXcode122App.swift
+//  SourceControlXcode122
+//
+//  Created by Charles Oladimeji on 5/18/22.
+//
+
+import SwiftUI
+
+@main
+struct SourceControlXcode122App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
